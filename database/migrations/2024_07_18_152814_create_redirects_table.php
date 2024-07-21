6ed53dac053a53ff6759b5f4334e01869412e825
”Egor <egorzharikov92@yandex.ru>
2024-07-22 01:16:23 +0700

    update migration redirects

--- database/migrations/2024_07_18_152814_create_redirects_table.php
+++ database/migrations/2024_07_18_152814_create_redirects_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('redirects', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('subscription_id');
+            $table->unsignedBigInteger('fee_id');
             $table->timestamps();
+
+            $table->index('subscription_id', 'redirect_subscription_idx');
+            $table->index('fee_id', 'redirect_fee_idx');
+
+            $table->foreign('subscription_id', 'redirect_subscription_fk')->references('id')->on('subscriptions')->cascadeOnDelete();
+            $table->foreign('fee_id', 'redirect_fee_fk')->references('id')->on('fees')->cascadeOnDelete();
         });
     }
 
