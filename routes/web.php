bdc1fc6ced93f9a5100a4a8ecfbef2e82aed5921
”Egor <egorzharikov92@yandex.ru>
2024-07-22 01:21:50 +0700

    update routes/web

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OfferController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/offers', [OfferController::class, 'index'])->name('offers');
+Route::get('/offers', [OfferController::class, 'index'])->name('offer.index');
+Route::get('/offers/create', [OfferController::class, 'create'])->name('offer.create');
+Route::post('/offers', [OfferController::class, 'store'])->name('offer.store');
+Route::get('/offers/{offer}', [OfferController::class, 'show'])->name('offer.show');
 Auth::routes(['verify' => true]);
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('verified')->name('dashboard');
