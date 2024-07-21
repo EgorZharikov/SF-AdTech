8241df013efe1085ada10fd54129a52ffbb5233a
”Egor <egorzharikov92@yandex.ru>
2024-07-22 01:08:20 +0700

    update model Subscription

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
     use HasFactory;
+
+    protected $guarded = false;
+
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class, 'offer_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function redirect()
+    {
+        return $this->hasMany(Redirect::class, 'subscription_id', 'id');
+    }
 }
