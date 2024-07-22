30a0eef8691b4f1770566c624192f0ec86b55ae9
Egor Zharikov <egorzharikov92@yandex.ru>
2024-07-22 18:08:50 +0700

    add SubscriptionController

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use Illuminate\Support\Str;
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+class SubscriptionController extends Controller
+{
+    public function create()
+    {
+        return view('offer.create');
+    }
+
+    public function store(Offer $offer)
+    {
+        $isSubscribed = Subscription::find(Auth::id())->user_id()->where('offer_id', $offer->id);
+        if($isSubscribed) {
+            return redirect()->route('offer.show', $offer->id)->with('error', 'you already subscribe');
+        } else {
+            Subscription::create([
+                'user_id' => Auth::id(),
+                'offer_id' => $offer->id,
+                'referal_link' => Str::random(36),
+                'status' => true,
+            ]);
+            return redirect()->route('offer.index');
+        }
+        
+    }
+
+    public function show()
+    {
+        
+    }
+}
