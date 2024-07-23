05af8b279e806980ab888a971aa65c89a8cad951
”Egor <egorzharikov92@yandex.ru>
2024-07-24 00:12:16 +0700

    Add RedirectController

---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use App\Models\Redirect;
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+
+class RedirectController extends Controller
+{
+    public function redirect($referal_link, Request $request)
+    {
+        $subscription = Subscription::where('referal_link', $referal_link)->with('offer')->first();
+        $offerStatus = $subscription->offer->status;
+        $uniqueIpOnly = $subscription->offer->unique_ip;
+        $clientIp = ip2long($request->getClientIp());
+        if ($offerStatus) {
+            if($uniqueIpOnly) {
+            $isUniqueIp = Redirect::where('ip', $clientIp)->doesntExist();
+            if ($isUniqueIp) {
+                Redirect::create([
+                    'subscription_id' => $subscription->id,
+                    'fee_id' => 1,
+                    'ip' => $clientIp,
+                    'status' => true,
+                ]);
+            } else {
+                Redirect::create([
+                    'subscription_id' => $subscription->id,
+                    'fee_id' => 1,
+                    'ip' => $clientIp,
+                    'status' => false,
+                ]);
+            }
+        } else {
+                Redirect::create([
+                    'subscription_id' => $subscription->id,
+                    'fee_id' => 1,
+                    'ip' => $clientIp,
+                    'status' => true,
+                ]);
+        }
+        } else {
+            Redirect::create([
+                'subscription_id' => $subscription->id,
+                'fee_id' => 1,
+                'ip' => $clientIp,
+                'status' => false,
+            ]);
+        }
+
+    }
+}
