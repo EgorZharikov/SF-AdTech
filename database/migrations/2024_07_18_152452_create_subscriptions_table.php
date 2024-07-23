f1eb7bf38346f9447a77f3dd05702cec3ac03eb8
”Egor <egorzharikov92@yandex.ru>
2024-07-24 00:10:04 +0700

    Update migration create_subsctiption

--- database/migrations/2024_07_18_152452_create_subscriptions_table.php
+++ database/migrations/2024_07_18_152452_create_subscriptions_table.php
@@ -16,11 +16,12 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('offer_id');
             $table->string('referal_link')->unique();
-            $table->boolean('status')->default(true);
+            // $table->boolean('status')->default(true);
             $table->timestamps();
 
             $table->index('user_id', 'subscription_user_idx');
             $table->index('offer_id', 'subscription_offer_idx');
+            $table->index('referal_link', 'referal_link_idx');
 
             $table->foreign('user_id', 'subscription_user_fk')->references('id')->on('users')->cascadeOnDelete();
             $table->foreign('offer_id', 'subscription_offer_fk')->references('id')->on('offers')->cascadeOnDelete();
