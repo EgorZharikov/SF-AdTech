608f52411e24e1727218f047b76f24df5d1c835c
”Egor <egorzharikov92@yandex.ru>
2024-07-24 00:11:30 +0700

    Update routes/web

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\SubscriptionController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -13,6 +14,7 @@ Route::get('/offers/create', [OfferController::class, 'create'])->name('offer.cr
 Route::post('/offers', [OfferController::class, 'store'])->name('offer.store');
 Route::get('/offers/{offer}', [OfferController::class, 'show'])->name('offer.show');
 Route::post('/offers/{offer}/subscriptions', [SubscriptionController::class, 'store'])->middleware('webmaster')->name('offer.subscription.store');
+Route::get('/redirect/{referal_link}', [RedirectController::class, 'redirect'])->middleware('subscribed')->name('redirect');
 
 Auth::routes(['verify' => true]);
 Route::post('/subscriptions', [SubscriptionController::class, 'store'])->name('subscription.store');
