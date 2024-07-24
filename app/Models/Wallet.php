d19f601abb9a86488ced6832b53143206d9e6acc
”Egor <egorzharikov92@yandex.ru>
2024-07-24 23:54:15 +0700

    Update Models/Wallet

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -13,6 +13,6 @@ class Wallet extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'wallet_id', 'id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
