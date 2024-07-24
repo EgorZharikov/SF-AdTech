6788247fc5fbbc6c2b61308cf02d327a3eb65a60
”Egor <egorzharikov92@yandex.ru>
2024-07-24 23:55:00 +0700

    Update RedirectService

--- app/Services/RedirectService.php
+++ app/Services/RedirectService.php
@@ -2,10 +2,11 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use App\Models\Offer;
+use App\Models\Wallet;
 use App\Models\Redirect;
 use App\Models\Subscription;
-use App\Models\User;
-use App\Models\Wallet;
 use \Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -26,6 +27,13 @@ class RedirectService
         $clientIp = ip2long($request->getClientIp());
         $data = ['subscription_id' => $subscription->id, 'ip' => $clientIp];
         $isUniqueIp = Redirect::where('ip', $clientIp)->doesntExist();
+        $subscription = Subscription::where('referal_link', $referal_link)->with('user.wallet')->first();
+        $webmasterWalletId = $subscription->user->wallet->id;
+        $offer = Offer::find($subscription->offer_id)->with('user.wallet')->first();
+        $advertiserWalletId = $offer->user->wallet->id;
+        $offer_award = $offer->award;
+        $fee = Fee::find($subscription->user->fee_id)->percent;
+        
         if ($offerStatus) {
             if ($uniqueIpOnly) {
                 if ($isUniqueIp) {
@@ -72,8 +80,8 @@ class RedirectService
     private function store(array $data, bool $status, $referal_link)
     {
         if($status) {
-            $advertiserWalletId = Subscription::where('') 
-            WalletService::update()
+            
+            
         }
         Redirect::create([
             'subscription_id' => $data['subscription_id'],
