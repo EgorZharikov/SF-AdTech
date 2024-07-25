f84ffabbc46d9868dcfa39e81105b54170359b12
Egor Zharikov <egorzharikov92@yandex.ru>
2024-07-25 18:04:28 +0700

    update model Wallet

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Wallet extends Model
@@ -14,5 +15,10 @@ class Wallet extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function walletTransactions()
+    {
+        return $this->hasMany(WalletTransaction::class, 'wallet_id', 'id');
     }
 }
