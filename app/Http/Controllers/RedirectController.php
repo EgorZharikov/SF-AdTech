1701805a54e9cbe41161e293d0194e8356862a57
Egor Zharikov <egorzharikov92@yandex.ru>
2024-07-25 18:02:57 +0700

    update RedirectController

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -6,57 +6,13 @@ use App\Models\Fee;
 use App\Models\Offer;
 use App\Models\Redirect;
 use App\Models\Subscription;
+use App\Services\RedirectService;
 use Illuminate\Http\Request;
 
 class RedirectController extends Controller
 {
     public function redirect($referal_link, Request $request)
     {
-        
-        $subscription = Subscription::where('referal_link', $referal_link)->with('user.wallet')->first();
-        $webmasterWalletId = $subscription->user->wallet->id;
-        $offer = Offer::find($subscription->offer_id)->with('user.wallet')->first();
-        $advertiserWalletId = $offer->user->wallet->id;
-        $fee = Fee::find($subscription->user->fee_id)->percent;
-        dd($fee);
-        $subscription = Subscription::where('referal_link', $referal_link)->with('offer')->first();
-        $offerStatus = $subscription->offer->status;
-        $uniqueIpOnly = $subscription->offer->unique_ip;
-        $clientIp = ip2long($request->getClientIp());
-        if ($offerStatus) {
-            if($uniqueIpOnly) {
-            $isUniqueIp = Redirect::where('ip', $clientIp)->doesntExist();
-            if ($isUniqueIp) {
-                Redirect::create([
-                    'subscription_id' => $subscription->id,
-                    'fee_id' => 1,
-                    'ip' => $clientIp,
-                    'status' => true,
-                ]);
-            } else {
-                Redirect::create([
-                    'subscription_id' => $subscription->id,
-                    'fee_id' => 1,
-                    'ip' => $clientIp,
-                    'status' => false,
-                ]);
-            }
-        } else {
-                Redirect::create([
-                    'subscription_id' => $subscription->id,
-                    'fee_id' => 1,
-                    'ip' => $clientIp,
-                    'status' => true,
-                ]);
-        }
-        } else {
-            Redirect::create([
-                'subscription_id' => $subscription->id,
-                'fee_id' => 1,
-                'ip' => $clientIp,
-                'status' => false,
-            ]);
-        }
-
+        return $redirector = ( new RedirectService())->process($referal_link, $request);
     }
 }
