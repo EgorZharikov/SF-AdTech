73aaa3c42b842956f4e6f57e09e68421df3e45c2
Egor Zharikov <egorzharikov92@yandex.ru>
2024-07-26 16:53:28 +0700

    add dashboard/webmaster

---
+++ resources/views/dashboard/webmaster/subscriptions.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="col-sm-3 m-3">
+    <div class="card" style="width: 20rem;">
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item text-success">Redirection award: ₽</li>
+            <li class="list-group-item">Topic: </li>
+        </ul>
+        <div class="card-body">
+            <a href="" class="btn btn-primary">Explore</a>
+        </div>
+    </div>
+</div>
+@endsection
