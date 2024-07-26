25ca2e99e57f4be39df244a01b3f2efdbf2078af
Egor Zharikov <egorzharikov92@yandex.ru>
2024-07-26 16:52:20 +0700

    update routes/web

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ Route::get('/offers/create', [OfferController::class, 'create'])->name('offer.cr
 Route::post('/offers', [OfferController::class, 'store'])->name('offer.store');
 Route::get('/offers/{offer}', [OfferController::class, 'show'])->name('offer.show');
 Route::post('/offers/{offer}/subscriptions', [SubscriptionController::class, 'store'])->middleware('webmaster')->name('offer.subscription.store');
+Route::delete('/offers/{offer}/subscriptions', [SubscriptionController::class, 'destroy'])->middleware('webmaster')->name('offer.subscription.destroy');
 Route::get('/redirect/{referal_link}', [RedirectController::class, 'redirect'])->middleware('subscribed')->name('redirect');
 
 Auth::routes(['verify' => true]);
-Route::post('/subscriptions', [SubscriptionController::class, 'store'])->name('subscription.store');
+// Route::post('/subscriptions', [SubscriptionController::class, 'store'])->name('subscription.store');
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('verified')->name('dashboard');
-
+Route::get('/dashboard/webmaster', [DashboardController::class, 'webmaster'])->middleware('verified')->name('dashboard.webmaster');
