cb0ad4618ac0a4d82abcd47267db08edac85926f
”Egor <egorzharikov92@yandex.ru>
2024-07-29 00:36:04 +0700

    update Models/Redirec

--- app/Models/Redirect.php
+++ app/Models/Redirect.php
@@ -9,11 +9,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Redirect extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = false;
 
     public function subscription()
     {
         return $this->belongsTo(Subscription::class, 'subscription_id', 'id');
+    }
+
+    public function fee()
+    {
+        return $this->belongsTo(Fee::class, 'fee_id', 'id');
     }
 }
