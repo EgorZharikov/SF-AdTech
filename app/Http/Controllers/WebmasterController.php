072d5261bd928f91b5237bf04bed1fcf530d4dd4
”Egor <egorzharikov92@yandex.ru>
2024-07-29 00:44:16 +0700

    add WebmasterController

---
+++ app/Http/Controllers/WebmasterController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fee;
+use App\Models\User;
+use App\Models\Wallet;
+use App\Models\Redirect;
+use App\Models\Subscription;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class WebmasterController extends Controller
+{
+    public function index()
+    {
+        return view('dashboard.profile');
+    }
+
+    public function subscriptions()
+    {
+        $subscriptions = Subscription::where('user_id', Auth::id())->with('offer')->get();
+        return view('dashboard.webmaster.subscriptions', compact('subscriptions'));
+    }
+
+    public function statistics()
+    {
+        $subscriptions = Subscription::where('user_id', Auth::id())->with('redirect')->get();
+        $statistics = [];
+        foreach ($subscriptions as $subscription) {
+            $income = 0;
+
+            foreach ($subscription->redirect as $redirect) {
+                $award = $redirect->subscription->offer->award;
+                $fee = Fee::where('id', $redirect->fee_id)->first()->percent;
+                $fee = $award * ($fee / 100);
+                $income += $award - $fee;
+            }
+
+            $statistics[] = ['subscription' => $subscription->referal_link, 'redirect_count' => $subscription->redirect->count(), 'award' => $income];
+        }
+
+        return view('dashboard.webmaster.statistics', compact('statistics'));
+    }
+
+    public function wallet()
+    {
+        $wallet = Wallet::where('user_id', Auth::id())->first();
+        return view('dashboard.webmaster.wallet', compact('wallet'));
+    }
+
+    public function profile()
+    {
+        $user = User::where('id', Auth::id())->with('fee')->first();
+        return view('dashboard.webmaster.profile', compact('user'));
+    }
+}
