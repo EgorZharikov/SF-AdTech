9af53edacbedeb25fb7ebf440903fe37176aba3c
”Egor <egorzharikov92@yandex.ru>
2024-07-29 00:39:10 +0700

    update migration create_redirects_table

--- database/migrations/2024_07_18_152814_create_redirects_table.php
+++ database/migrations/2024_07_18_152814_create_redirects_table.php
@@ -14,12 +14,16 @@ return new class extends Migration
         Schema::create('redirects', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('subscription_id');
+            $table->unsignedBigInteger('fee_id');
             $table->integer('ip')->unsigned();
             $table->boolean('status');
             $table->timestamps();
+            $table->softDeletes();
 
             $table->index('subscription_id', 'redirect_subscription_idx');
             $table->foreign('subscription_id', 'redirect_subscription_fk')->references('id')->on('subscriptions')->cascadeOnDelete();
+            $table->index('fee_id', 'redirect_fee_idx');
+            $table->foreign('fee_id', 'redirect_fee_fk')->references('id')->on('fees');
         });
     }
 
