57e97e16caba6df73636c4fbe25a745ee57613df
”Egor <egorzharikov92@yandex.ru>
2024-07-30 00:29:59 +0700

    add view  advertiser.index

---
+++ resources/views/dashboard/advertiser/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.dashboard')
+
+@section('sidebar')
+@include('includes.dashboard.advertiser.sidebar')
+@endsection
+
+@section('title')
+<h3 class="mb-0">Advertiser</h3>
+@endsection
+
+@section('content')
+
+@endsection
