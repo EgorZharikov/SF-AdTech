3db541284451cf634ee7dd5dfa39dd8d94ec911d
”Egor <egorzharikov92@yandex.ru>
2024-07-30 00:30:24 +0700

    add view  webmaster.index

---
+++ resources/views/dashboard/webmaster/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.dashboard')
+
+@section('sidebar')
+    @include('includes.dashboard.webmaster.sidebar')
+@endsection
+
+@section('title')
+    <h3 class="mb-0">Webmaster</h3>
+@endsection
+
+@section('content')
+    
+@endsection
