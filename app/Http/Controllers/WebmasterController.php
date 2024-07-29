90b5d373c95409fb873c1c70f49c2ffa74b665f8
”Egor <egorzharikov92@yandex.ru>
2024-07-30 00:21:30 +0700

    update  WebmasterController

--- app/Http/Controllers/WebmasterController.php
+++ app/Http/Controllers/WebmasterController.php
@@ -14,7 +14,7 @@ class WebmasterController extends Controller
 {
     public function index()
     {
-        return view('dashboard.profile');
+        return view('dashboard.webmaster.index');
     }
 
     public function subscriptions()
