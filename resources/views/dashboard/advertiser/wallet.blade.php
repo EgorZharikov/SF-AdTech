cf0b8d8a15e15ceaa7a02bdc0c7b8ebe8455a6ce
”Egor <egorzharikov92@yandex.ru>
2024-07-30 23:51:27 +0700

    update view advertiser.wallet

--- resources/views/dashboard/advertiser/wallet.blade.php
+++ resources/views/dashboard/advertiser/wallet.blade.php
@@ -1,36 +1,46 @@
 @extends('layouts.dashboard')
 
 @section('sidebar')
-@include('includes.dashboard.advertiser.sidebar')
+    @include('includes.dashboard.advertiser.sidebar')
 @endsection
 
 @section('title')
-<h3 class="mb-0">Wallet</h3>
+    <h3 class="mb-0">Wallet</h3>
 @endsection
 
 @section('content')
-<div class="container text-center">
-    <div class="row align-items-start">
-        <div class="col-sm-3 m-3">
-            <div class="card d-flex justify-content-center bg-dark" style="width: 20rem;">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item text-bg-success">
-                        <h4>Balance: {{$wallet->balance}} ₽</h4>
-                    </li>
-                </ul>
-                <div class="card-body d-flex justify-content-center" style="width: 20rem;">
-                    <input type="number" class="form-control px-5" aria-label="Amount" name="award" value="" step="1" pattern="\d*">
-                </div>
-                <div class="card-body d-flex justify-content-center">
-                    <form method="post" action="">
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <div class="col-sm-3 m-3">
+                <div class="card d-flex justify-content-center bg-dark" style="width: 20rem;">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item text-bg-success">
+                            <h4>Balance: {{ $wallet->balance }} ₽</h4>
+                        </li>
+                    </ul>
+                    <form method="post" action="{{ route('wallet.update') }}">
                         @csrf
-                        <input type="hidden" name="offer_id" value="">
-                        <button type="submit" class="btn btn-outline-success px-4 me-4">Replenish</button>
+                        @method('patch')
+                        <div class="card-body d-flex justify-content-center" style="width: 20rem;">
+                            <input type="number" class="form-control px-5" aria-label="Amount" name="amount"
+                                value="" step="1" pattern="\d*">
+                        </div>
+                        <div class="card-body d-flex justify-content-center">
+                            <button type="submit" name="replenish"
+                                class="btn btn-outline-success px-4 me-4">Replenish</button>
+                            <button type="submit" name="withdraw"
+                                class="btn btn-outline-success px-4 me-4">Withdraw</button>
+                        </div>
                     </form>
-                    <a href="" class=" btn btn-outline-success">Withdraw</a>
                 </div>
             </div>
+            @if($errors->has('walletError'))
+            <div class="col-sm-3 m-3">
+                <div class="alert alert-warning" role="alert">
+                    {{$errors->first()}}
+                </div>
+            </div>
+            @endif
         </div>
     </div>
-</div>
 @endsection
