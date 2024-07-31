ea9bf6adca53d5eaaaa34aed7c85944d8f66a1a6
Egor Zharikov <egorzharikov92@yandex.ru>
2024-07-31 17:59:56 +0700

    update view advertiser.statistics

--- resources/views/dashboard/advertiser/statistics.blade.php
+++ resources/views/dashboard/advertiser/statistics.blade.php
@@ -48,4 +48,46 @@
         </table>
     </div>
 </div>
+<div class="container">
+    <div class="row align-items-center d-inline-flex">
+        <div class="justify-content-center d-inline-flexs">
+            <form method="post" action="{{ url('/advertiser/statistics') }}">
+                @csrf
+                <input type="date" name="date" value="{{date('Y-m-d')}}" class="m-4" style="transform: scale(1.3);" />
+                <button type="submit" name="day" class="btn btn-outline-dark ms-1">Day</button>
+                <button type="submit" name="month" class="btn btn-outline-dark ms-1">Month</button>
+                <button type="submit" name="year" class="btn btn-outline-dark ms-1">Year</button>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row align-items-center">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Offer title</th>
+                    <th scope="col">Subscriptions now</th>
+                    <th scope="col">Subscriptions total</th>
+                    <th scope="col">Redirects</th>
+                    <th scope="col">Redirect award</th>
+                    <th scope="col">Costs ₽</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <th scope="row">Σ</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
