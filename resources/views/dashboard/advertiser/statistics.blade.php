7c9f08362ef0c9e728b4883d8c1dffd48cc2bad9
Egor Zharikov <egorzharikov92@yandex.ru>
2024-08-01 18:03:14 +0700

    update view advertiser.statistic

--- resources/views/dashboard/advertiser/statistics.blade.php
+++ resources/views/dashboard/advertiser/statistics.blade.php
@@ -53,7 +53,7 @@
         <div class="justify-content-center d-inline-flexs">
             <form method="post" action="{{ url('/advertiser/statistics') }}">
                 @csrf
-                <input type="date" name="date" value="{{date('Y-m-d')}}" class="m-4" style="transform: scale(1.3);" />
+                <input type="date" name="date" value="{{$userDate}}" class="m-4" style="transform: scale(1.3);" />
                 <button type="submit" name="day" class="btn btn-outline-dark ms-1">Day</button>
                 <button type="submit" name="month" class="btn btn-outline-dark ms-1">Month</button>
                 <button type="submit" name="year" class="btn btn-outline-dark ms-1">Year</button>
@@ -80,14 +80,14 @@
                 <tr>
                     <td>{{$dayOffer->id}}</td>
                     <td>{{$dayOffer->title}}</td>
-                    <td>{{$dayOffer->subscriptions_count}}</td>
-                    <td>{{$dayOffer->subscriptions_count}}</td>
+                    <td>{{$dayOffer->subscriptionsNow}}</td>
+                    <td>{{$dayOffer->subscriptionsNow}}</td>
                     <td>{{$dayOffer->redirectsCount}}</td>
                     <td>{{$dayOffer->award}}</td>
                     <th scope="row">{{$dayOffer->redirectsCount * $dayOffer->award}}</th>
                 </tr>
-                 @endforeach
-                  <tr>
+                @endforeach
+                <tr>
                     <td></td>
                     <td></td>
                     <td></td>
